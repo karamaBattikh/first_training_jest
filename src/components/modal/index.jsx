dca7a720a3e6c37fb576f0a64bad2dc8ca2598a3
karamaBattikh <battikh.karama@gmail.com>
2022-06-20 16:22:01 +0200

    init commit

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Modal = ({title, children, onClick}) => (
+    <div>
+        <div className='conetent'>
+        <div className='conetent'>
+
+</div>
+        </div>
+
+    </div>
+)
