24689c8b9e2b1418ccffc9283721530f58679c2f
karamaBattikh <battikh.karama@gmail.com>
2022-06-24 10:20:23 +0200

    add folder screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { useEffect, useState } from 'react';
-
-import List from 'components/list';
-import Modal from 'components/modal';
-import useToggle from 'hooks/useToggle';
-import fetchUser from 'services/fetchUser';
+import Home from 'screens/home';
 
 import './App.css';
 
 function App() {
-  const [toggle, handleToggle] = useToggle(false);
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const handle = async () => {
-      const list = await fetchUser();
-      console.log('*user*', list);
-      setUsers(list);
-    };
-    handle();
-  }, []);
-
   return (
     <div className="app">
-      <button className="app__button" onClick={() => handleToggle(true)}>
-        open Modal
-      </button>
-      <Modal isShowing={toggle} title="Bonjour!" onClose={() => handleToggle(false)}>
-        karama
-      </Modal>
-      {users.length > 0 && <List list={users} />}
+      <Home />
     </div>
   );
 }
---
+++ src/screens/home/index.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+import List from 'components/list';
+import Modal from 'components/modal';
+import useToggle from 'hooks/useToggle';
+import fetchUser from 'services/fetchUser';
+
+import styles from './home.module.scss';
+
+const Home = () => {
+  const [toggle, handleToggle] = useToggle(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const handle = async () => {
+      const list = await fetchUser();
+      console.log('*user*', list);
+      setUsers(list);
+    };
+    handle();
+  }, []);
+
+  return (
+    <div>
+      <button className={styles.home__button} onClick={() => handleToggle(true)}>
+        open Modal
+      </button>
+      <Modal isShowing={toggle} title="Bonjour!" onClose={() => handleToggle(false)}>
+        karama
+      </Modal>
+      {users.length > 0 ? (
+        <>
+          <h1 className={styles.home__title}>User</h1>
+          <List list={users} />
+        </>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
+};
+
+export default Home;
